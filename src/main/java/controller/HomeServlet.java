69d4354f00657f8d9dcefa774d7348464e1d3e4e
unknown <thaovtphe182556@fpt.edu.vn>
2026-06-07 04:22:04 +0700

    feed: tao home page

--- src/main/java/controller/HomeServlet.java
+++ src/main/java/controller/HomeServlet.java
@@ -1,19 +1,18 @@
 package controller;
 
-import dal.MovieDAO;
-import model.entity.Genre;
-import model.entity.Movie;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
+import dal.MovieDAO;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
-
+import model.entity.Genre;
+import model.entity.Movie;
+// Servlet xử lý trang chủ, hiển thị các bộ phim nổi bật, đang chiếu và sắp chiếu
 @WebServlet(urlPatterns = {"/home"})
 public class HomeServlet extends HttpServlet {
 
